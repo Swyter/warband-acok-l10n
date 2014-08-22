8a9bcf6b9db3391c86904de5387a419b2290c8b3
Swyter <Swyter@Portatil>
2014-08-22 20:38:43 +0200

    *first revision with the stuff Fantu gave me. -- acok 1.31 english language: fantu.eu/warband/acok_en.7z

---
+++ tx.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Updating translations from Transifex..."
+
+# convert everything to Joomla INI format
+luajit tx.lua convert
+
+# push our latest strings to the web
+# tx push -s -t -f --skip --no-interactive
+# tx push -s -f --skip --no-interactive
+# tx push -s -t -f --skip --no-interactive
+# tx push -t -l sv --skip --no-interactive
+
+# pull latest translations
+tx pull -a -f --skip --minimum-perc=0
+
+# revert back to mab format
+luajit tx.lua revert
+
+echo "Done!"
